318f1af2650f13c67a7583f19627476e4742b6b3
Gatlin C Johnson <rokenrol@gmail.com>
2012-04-22 15:27:52 -0500

    go fmt

--- src/saturn/main.go
+++ src/saturn/main.go
@@ -1,59 +1,59 @@
 package main
 
 import (
-    "fmt"
-    "net"
-    "minisat"
-    "bytes"
+	"fmt"
+	"net"
+	"minisat"
+	"bytes"
 )
 
 func main() {
-    /* listen on a port for incoming problems */
-    ln, err := net.Listen("tcp",":1888")
-    if err != nil {
-        fmt.Printf("ERROR on listen\n")
-    }
+	/* listen on a port for incoming problems */
+	ln, err := net.Listen("tcp", ":1888")
+	if err != nil {
+		fmt.Printf("ERROR on listen\n")
+	}
 
-    defer ln.Close()
+	defer ln.Close()
 
-    /* each request gets a goroutine */
-    for {
-        conn, err := ln.Accept()
-        if err != nil {
-            fmt.Printf("ERROR on accept")
-        }
-        go handleConnection(conn)
-    }
+	/* each request gets a goroutine */
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Printf("ERROR on accept")
+		}
+		go handleConnection(conn)
+	}
 }
 
 func handleConnection(c net.Conn) {
-    buffer := make([]byte,2048)
+	buffer := make([]byte, 2048)
 
-    _, err := c.Read(buffer)
-    if err != nil {
-        fmt.Printf("ERROR on read\n")
-    }
+	_, err := c.Read(buffer)
+	if err != nil {
+		fmt.Printf("ERROR on read\n")
+	}
 
-    solvable, solution := minisat.SolveDIMACS(string(buffer))
-    if solvable {
-        c.Write(writeSolution(solution))
-    } else {
-        c.Write([]byte("0\n"))
-    }
-    c.Close()
+	solvable, solution := minisat.SolveDIMACS(string(buffer))
+	if solvable {
+		c.Write(writeSolution(solution))
+	} else {
+		c.Write([]byte("0\n"))
+	}
+	c.Close()
 }
 
 func writeSolution(b []bool) []byte {
-    buffer := bytes.NewBufferString("");
-    fmt.Fprint(buffer,"1\n")
-    for variable,val := range b {
-        var t int
-        if val {
-            t = 1
-        } else {
-            t = 0
-        }
-        fmt.Fprintf(buffer,"%d %d\n",variable+1,t)
-    }
-    return buffer.Bytes()
+	buffer := bytes.NewBufferString("")
+	fmt.Fprint(buffer, "1\n")
+	for variable, val := range b {
+		var t int
+		if val {
+			t = 1
+		} else {
+			t = 0
+		}
+		fmt.Fprintf(buffer, "%d %d\n", variable+1, t)
+	}
+	return buffer.Bytes()
 }
